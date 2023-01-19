685ac7167a0f86bfd2d671173b30429d9914e921
Thiago César dos Reis Oliveira <thiafa.vos@gmail.com>
2023-01-19 16:49:49 -0300

    Criando meu próprio filter

---
+++ 09-array-filter/index.js
@@ -0,0 +1,38 @@
+const { obterPessoas } = require('./service');
+
+Array.prototype.meuFilter = function (callback) {
+  const lista = [];
+  for (index in this) {
+    const item = this[index];
+    const result = callback(item, index, this);
+    if (!result) continue;
+    lista.push(item);
+  }
+  return lista;
+};
+
+async function main() {
+  try {
+    const { results } = await obterPessoas('a');
+    // const familyLars = results.filter(function (item) {
+    //   // por padrão precisa retornar um booleano para informar se deve manter ou remover da lista
+    //   // false > remove da lista
+    //   // true > mantém
+    //   // Não encontrou = -1
+    //   // encontrou = posição do array
+    //   const result = item.name.toLowerCase().indexOf(`lars`) !== -1;
+    //   return result;
+    // });
+
+    const familyLars = results.meuFilter((item, index, lista) => {
+      console.log(`index: ${index}`, lista.length);
+      return item.name.toLowerCase().indexOf('lars') !== -1;
+    });
+    const names = familyLars.map((pessoa) => pessoa.name);
+    console.log(names);
+  } catch (error) {
+    console.error('ERRO INTERNO!', error);
+  }
+}
+
+main();
