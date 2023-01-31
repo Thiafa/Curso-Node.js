2f930f4f3a93c7f8c08429705da6eefdd964b584
Thiago César dos Reis Oliveira <thiafa.vos@gmail.com>
2023-01-31 11:40:33 -0300

    Manipulando arquivos - READ

---
+++ 12-cli/database.js
@@ -0,0 +1,22 @@
+const { readFile } = require('fs');
+const { promisify } = require('util');
+
+const readFileAsync = promisify(readFile);
+
+class Database {
+  constructor() {
+    this.NOME_ARQUIVO = 'herois.json';
+  }
+  async obterDadosArquivos() {
+    const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf-8');
+    return JSON.parse(arquivo.toString());
+  }
+  escreverArquivo() {}
+  async listar(id) {
+    const dados = await this.obterDadosArquivos();
+    const dadosFitlrados = dados.filter((item) => (id ? item.id === id : true));
+    return dadosFitlrados;
+  }
+}
+
+module.exports = new Database();
