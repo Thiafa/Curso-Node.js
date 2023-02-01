f050cd050518cac20f60b49ce948329e67c4dc85
Thiago César dos Reis Oliveira <thiafa.vos@gmail.com>
2023-02-01 13:56:04 -0300

    Manipulando arquivos - Create

--- 12-cli/database.js
+++ 12-cli/database.js
@@ -1,7 +1,8 @@
-const { readFile } = require('fs');
+const { readFile, writeFile } = require('fs');
 const { promisify } = require('util');
 
 const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 class Database {
   constructor() {
@@ -11,7 +12,24 @@ class Database {
     const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf-8');
     return JSON.parse(arquivo.toString());
   }
-  escreverArquivo() {}
+  async cadastrarHeroi(heroi) {
+    const dados = await this.obterDadosArquivos();
+    const id = heroi.id <= 2 ? heroi.id : Date.now();
+    const heroiComId = {
+      id,
+      ...heroi,
+    };
+    const dadosFinal = [...dados, heroiComId];
+    const resultado = await this.escreverArquivo(dadosFinal);
+    return resultado;
+  }
+  async escreverArquivo(dados) {
+    const arquivo = await writeFileAsync(
+      this.NOME_ARQUIVO,
+      JSON.stringify(dados),
+    );
+    return true;
+  }
   async listar(id) {
     const dados = await this.obterDadosArquivos();
     const dadosFitlrados = dados.filter((item) => (id ? item.id === id : true));
--- 12-cli/test.js
+++ 12-cli/test.js
@@ -9,16 +9,21 @@ const DEFAULT_ITEM_CADASTRAR = {
 };
 
 describe('Suíte de manipulação de Herois', () => {
+  beforeEach(async () => [
+    await database.cadastrarHeroi(DEFAULT_ITEM_CADASTRAR),
+  ]);
   it('deve pesquisar um heroi, usando arquivos', async () => {
     const expected = DEFAULT_ITEM_CADASTRAR;
     const [resultado] = await database.listar(expected.id);
 
     deepEqual(resultado, expected);
   });
-  // it('deve cadastrar um heroi, usando arquivos', async () => {
-  //   const expected = {
-  //     DEFAULT_ITEM_CADASTRAR,
-  //   };
-  //   ok(null, expected);
-  // });
+  it('deve cadastrar um heroi, usando arquivos', async () => {
+    const expected = {
+      DEFAULT_ITEM_CADASTRAR,
+    };
+    const resultado = await database.cadastrarHeroi(DEFAULT_ITEM_CADASTRAR);
+    const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
+    ok(actual, expected);
+  });
 });
